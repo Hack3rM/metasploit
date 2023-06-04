9c4101a4e782cb2703865fca22f68c188f99cb8b
Hack3rM <135494364+Hack3rM@users.noreply.github.com>
2023-06-04 08:54:46 +0300

    Add files via upload

---
+++ mymetasploit.py
@@ -0,0 +1,81 @@
+#!/user/bin/python3
+
+import time
+
+print("""
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+|||            The king of Hacker's  ||||
+|||                   Mikinaol                ||||
+|||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+
+
+
+""")
+
+name = input ("what is your name: ")
+if name == "Khalid" or name == "khalid":
+               print ("fuck you")
+               exit()               
+else:
+               print("welocme " + name + "\n")
+               
+  
+ 
+
+name = input ("i am downlod metasplot yes/no: ")
+time.sleep (2)
+if name == "yes":
+               print("""
+               /////////////////////////////////////                            /s/t/a/r/t/i/n/g//d/o/w/n/l/o/a/d/          ///////""")
+               time.sleep (2)
+               print("""M E T A S P L O I T""")
+               time.sleep (2)
+               print("downloding .....8%")
+               time.sleep (2)
+               print("downloading.....22%")
+               time.sleep (2)
+               print("downloding.......37%")
+               time.sleep (3)
+               print("downloading.....67%")
+               time.sleep (4)
+               print("downloading.....89%")
+               time.sleep (4)
+               print("downloading.....95%")
+               time.sleep (5)
+               print("downloadimg...100%\n")
+               time.sleep (3)
+               print("""
+               D O W N L O A D I N G 
+               F I N IS H E D
+           """)
+           
+
+                      
+else:
+     print("try agen")
+     exit()
+print("downloding is finished\n" )
+time.sleep (2)
+name = input ("do you went to start metasplot ? [Y/n]: ")
+if name == "Y":
+     print(""" 
+     M T A S P L O I T E
+     """)
+     time.sleep (2)
+name = input ("m6>")
+if name == "search payload":
+    print("""
+    payload                             info            rank
+    1.android/meterpreter    payload   exelent """)
+else:
+    print("try agen")
+    name = input ("m6>")
+if name == "search payload":
+    print("""
+    payload                             info            rank
+    1.android/meterpreter    payload         exelent exelent """)
+    
+    
+      
+      
